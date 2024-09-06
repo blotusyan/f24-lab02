2551b96df1875faf7ca818a2ed511fcc8c68a99d
Weiheng <weihengyans@gmail.com>
2024-09-05 20:33:49 -0400

    enhance inheritance

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,17 @@
 package edu.cmu.cs.cs214.lab02;
 
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.Circle;
 
 public class Main {
     public static void main(String[] args) {
         Rectangle rectangle = new Rectangle(2, 3);
+        Circle circle = new Circle(2);
 
         Renderer renderer = new Renderer(rectangle);
+        Renderer renderer2 = new Renderer(circle);
 
         renderer.draw();
+        renderer2.draw();
     }
 }
