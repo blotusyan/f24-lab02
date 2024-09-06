5cb2394a7175d5386188f4a5dd629320223d2b45
Weiheng <weihengyans@gmail.com>
2024-09-06 13:20:19 -0400

    add java changes

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,18 @@
 package edu.cmu.cs.cs214.lab02;
 
+import edu.cmu.cs.cs214.lab02.shapes.Shape;
+import edu.cmu.cs.cs214.lab02.shapes.Circle;
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
+        Shape rectangle = new Rectangle(2, 3);
+        Shape circle = new Circle(2);
 
-        Renderer renderer = new Renderer(rectangle);
+        Renderer renderer_rectangle = new Renderer(rectangle);
+        Renderer renderer_circle = new Renderer(circle);
 
-        renderer.draw();
+        renderer_rectangle.draw();
+        renderer_circle.draw();
     }
 }
