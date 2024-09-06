9579d0e84974599a3f6af7961808e9dab20cba12
Weiheng <weihengyans@gmail.com>
2024-09-06 13:18:37 -0400

    resolve branch issue

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,6 +1,17 @@
 import { newRenderer } from "./renderer.js"
-import { Rectangle, newRectangle } from "./shapes/rectangle.js";
+import { Rectangle } from "./shapes/rectangle.js";
+import { Circle } from "./shapes/circle.js";
+import { Square } from "./shapes/square.js";
 
-const rectangle: Rectangle = newRectangle(2, 3)
-const renderer = newRenderer(rectangle)
-renderer.draw();
+const rectangle: Shape = new Rectangle(2, 3)
+const circle: Shape = new Circle(2)
+const square: Shape = new Square(2)
+
+const renderer_rectangle = newRenderer(rectangle)
+renderer_rectangle.draw();
+
+const renderer_circle = newRenderer(circle)
+renderer_circle.draw();
+
+const renderer_square = newRenderer(square)
+renderer_square.draw();
